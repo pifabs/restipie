c619b9fb9a2b55c549b12832ddb286d87146500a
palanskiheji <hejipalanski@yahoo.com>
2021-02-21 21:11:54 +0800

    feat: add 2fa login and conformation api

--- restipie/custom_apis/auth/controller.py
+++ restipie/custom_apis/auth/controller.py
@@ -1,10 +1,13 @@
 import frappe
+from frappe.exceptions import ValidationError, AuthenticationError
 
 from restipie.custom_api_core import request
 from restipie.custom_api_core import response
 from restipie.custom_api_core.middleware import authenticate, validate_schema
 
 from .schema import (
+	login_2fa_schema,
+	confirm_2fa_schema,
 	signup_schema,
 	simple_login_schema,
 	change_password_schema,
@@ -16,6 +19,8 @@ from . import service
 MIDDLEWARES = {
 	"signup": (validate_schema(signup_schema),),
 	"login": (validate_schema(simple_login_schema),),
+	"login_2fa": (validate_schema(login_2fa_schema),),
+	"confirm_2fa": (validate_schema(confirm_2fa_schema),),
 	"change_password": (authenticate, validate_schema(change_password_schema),),
 	"reset_password": (validate_schema(password_reset_request_schema),),
 	"logout": (authenticate,)
@@ -45,6 +50,23 @@ def login(*args, **kwargs):
 	return  response.JSONResponse(message="Login success!", data=user)
 
 
+@request.api("POST", "/v1/api/login/2fa", middlewares=MIDDLEWARES["login_2fa"])
+def login_2fa(*args, **kwargs):
+	result = service.login_2fa(data=kwargs.get("data"))
+	return response.JSONResponse(**result)
+
+
+@request.api("POST", "/v1/api/login/2fa/confirm", middlewares=MIDDLEWARES["confirm_2fa"])
+def confirm_2fa(*args, **kwargs):
+	try:
+		result = service.confirm_2fa(data=kwargs.get("data"))
+		return response.JSONResponse(message="Login success!", data=result)
+	except AuthenticationError as e:
+		raise AuthenticationError("Invalid email or verification code")
+	except Exception as e:
+		raise e
+
+
 @request.api("PATCH", "/v1/api/users/password", middlewares=MIDDLEWARES["change_password"])
 def change_password(*args, **kwargs):
 	try:
@@ -54,7 +76,7 @@ def change_password(*args, **kwargs):
 		raise e
 
 
-@request.api("POST", "/v1/api/request-password-reset", middlewares=MIDDLEWARES["reset_password"])
+@request.api("PUT", "/v1/api/users/password", middlewares=MIDDLEWARES["reset_password"])
 def reset_password(*args, **kwargs):
 	try:
 		result = service.handle_pwd_reset_request(**kwargs.get("data"))
